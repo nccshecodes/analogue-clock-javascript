c2347818bf356c839c920227b58a231b4b3697a6
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2017-12-21 20:09:06 +0000

    Hour hand moves incrementally in line with minute hand and minute hand moves incrementally in line with second hand, dividing one segment for hour (30 degrees) or minute (6 degrees) by number of minutes or seconds passed respectively. This means that there is no sudden jump as the hour or minute changes.

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
 // each hour moves 30deg (360/12)
 // each minute moves 6deg (360/60)
 // each second moves 6deg (360/60)
-let hrPosition = hr*30;
-let minPosition = min*6;
+let hrPosition = (hr*30)+(min*6/12); // moves hr hand gradually as minutes pass dividing 1 hour segment by
+let minPosition = (min*6)+(sec*6/60); // moves minute hand gradually as seconds pass
 let secPosition = sec*6;
-//that's current time set.
+//that's current time set, but it's static.
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
