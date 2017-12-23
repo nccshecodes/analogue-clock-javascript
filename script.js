1a3e20d9761a89c11de14569dff9071c43f2237d
Nathalie Christmann-Cooper <nathalie@shecodes.co.uk>
2017-12-23 10:01:56 +0000

    Reste script to preferred option.

--- script.js
+++ script.js
@@ -2,32 +2,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-// to get time 'now' pulling date, time and time zone pulled off the browser
-var date = new Date(); //date object
-
-//get hour, minutes and seconds from date object
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
-
-// each hour moves 30deg (360/12)
-// each minute moves 6deg (360/60)
-// each second moves 6deg (360/60)
-let hrPosition = (hr*30)+(min*30/60); // moves hr hand gradually as minutes pass dividing 1 hour segment by 60 minutes
-let minPosition = (min*6)+(sec*6/60); // moves minute hand gradually as seconds pass dividing 1 min section by 60 seconds
-let secPosition = sec*6;
-//that's current time set, but it's static.
-
 function runTheClock(){
 
-  /*
-  dividing hour segment by number of seconds in an hour
-  because script runs every second (30/3600)
-  */
-  hrPosition = hrPosition+(30/3600);
-  minPosition = minPosition+(6/60);
-  secPosition = secPosition+6;
+  // to get time 'now' pulling date, time and time zone pulled off the browser
+  var date = new Date(); //date object
+
+  //get hour, minutes and seconds from date object
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("Hour: " + hr + " Minute: " + min + " Second: " + sec);
+
+  // each hour moves 30deg (360/12)
+  // each minute moves 6deg (360/60)
+  // each second moves 6deg (360/60)
+  let hrPosition = (hr*30)+(min*30/60); // moves hr hand gradually as minutes pass dividing 1 hour segment by 60 minutes
+  let minPosition = (min*6)+(sec*6/60); // moves minute hand gradually as seconds pass dividing 1 min section by 60 seconds
+  let secPosition = sec*6;
+  //that's current time set, but it's static.
 
   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
